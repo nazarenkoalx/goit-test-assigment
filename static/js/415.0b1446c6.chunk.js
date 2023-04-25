423443ccebbe1e61660e831c864f324aa6c25fcc
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-04-25 22:53:00 +0300

    Updates

--- static/js/415.0b1446c6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_test_assigment=self.webpackChunkgoit_test_assigment||[]).push([[415],{5415:function(t,e,s){s.r(e);var n=s(184);e.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:" React"})})}}}]);
-//# sourceMappingURL=415.0b1446c6.chunk.js.map
